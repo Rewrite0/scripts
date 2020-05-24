bc9ffe631636eed2500ef38de78c088923c8fa5b
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-05-24 21:52:18 +0800

    --

--- imglink.sh
+++ imglink.sh
@@ -1,1 +1,6 @@
-echo "https://cdn.jsdelivr.net/gh/Rewrite0/FigureBed/img/$1"
+file=$(ls | sed "s:^:`pwd`/:" | grep $1)
+file_dir=${file#*Bed}
+dir=https://cdn.jsdelivr.net/gh/Rewrite0/FigureBed
+link=$dir$file_dir
+echo $link
+echo $link | xclip -selection clipboard
