fc66dcaaa66cfaf0da1b79edc2fdb31a8a298d26
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-05-29 21:32:37 +0800

    --

--- imglink.sh
+++ imglink.sh
@@ -1,6 +1,13 @@
 file=$(realpath $1)
+md=$2
 file_dir=${file#*Bed}
-dir=https://cdn.jsdelivr.net/gh/Rewrite0/FigureBed@master
-link=$dir$file_dir
-echo $link
-echo $link | xclip -selection clipboard
+url=https://cdn.jsdelivr.net/gh/Rewrite0/FigureBed@master
+link=$url$file_dir
+mdlink="![<++>]("$link")"
+if [ "$md" == "md" ];then
+	echo $mdlink
+	echo $mdlink | xclip -selection clipboard
+else
+	echo $link
+	echo $link | xclip -selection clipboard
+fi
