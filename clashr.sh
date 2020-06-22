3b14687f188e6e55cc4ff59f608a89b2e1c52ae2
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-06-22 23:10:18 +0800

    --

--- clashr.sh
+++ clashr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf ~/scripts/log/clashr.log
-nohup ~/prog/clashr/clashr-v0.19.0.2 > ~/scripts/log/clashr.log 2>&1 &
+nohup ~/prog/clashr/clashr > ~/scripts/log/clashr.log 2>&1 &
 sleep 3
 cat ~/scripts/log/clashr.log
