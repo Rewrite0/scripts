5609c90c2a6922b1a84a5a97832d596eecb623d3
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-07-14 18:14:37 +0800

    修改clashr脚本

--- clashr.sh
+++ clashr.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-rm -rf ~/scripts/log/clashr.log
-nohup ~/prog/clashr/clashr > ~/scripts/log/clashr.log 2>&1 &
-sleep 3
-cat ~/scripts/log/clashr.log
+#########################################################################
+# File Name: clashr.sh
+# Created Time: 2020年07月14日 星期二 18时04分23秒
+#########################################################################
+a=$1
+start(){
+	rm -rf ~/scripts/log/clashr.log
+	nohup ~/prog/clashr/clashr > ~/scripts/log/clashr.log 2>&1 &
+	sleep 3
+	cat ~/scripts/log/clashr.log
+}
+
+if [ -z $a ];then
+	start
+elif [ $a == restart ];then
+	pkill clashr
+	start
+elif [ $a == status ];then
+	cat ~/scripts/log/clashr.log
+fi
