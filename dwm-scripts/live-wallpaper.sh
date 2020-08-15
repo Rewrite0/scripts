f1e1a7c058f6cc5bf7e0bea50b0af1cd135e850c
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-08-15 21:08:34 +0800

    动态壁纸默认静音

--- dwm-scripts/live-wallpaper.sh
+++ dwm-scripts/live-wallpaper.sh
@@ -1,2 +1,3 @@
 rm -rf ~/scripts/log/xwinwrap.log
-nohup xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -shuffle -slave -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > ~/scripts/log/xwinwrap.log 2>&1 &
+mkfifo /tmp/wallpaper
+nohup xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -softvol -softvol-max 200 -shuffle -slave -quiet -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > ~/scripts/log/xwinwrap.log 2>&1 &
