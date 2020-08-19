5e7b69f05e2cc96dcc4d39bd4854e1f0fbce2004
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-08-20 05:40:31 +0800

    --

--- dwm-scripts/appstart.sh
+++ dwm-scripts/appstart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 picom -o 0.9 -i 0.6 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+xdman &
 ~/scripts/clash.sh
 fcitx5 &
-xdman &
 nm-applet &
 blueman-applet &
 wmname LG3D
