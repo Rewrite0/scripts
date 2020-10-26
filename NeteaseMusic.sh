40f4428fb0475502f1f8ca9c6a719e5e1fd0bc54
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-10-26 14:00:21 +0800

    --

---
+++ NeteaseMusic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#########################################################################
+# File Name: NeteaseMusic.sh
+# Created Time: 2020年10月25日 星期日 20时21分40秒
+#########################################################################
+#linux网易云音乐客户端使用UnblockNeteaseMusic需要添加"--ignore-certificate-errors"参数
+netease-cloud-music --ignore-certificate-errors
