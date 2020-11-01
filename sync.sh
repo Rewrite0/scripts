0c673cb26a26289ab82054123f1eee10a69b9570
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-11-02 00:09:51 +0800

    sync脚本添加完成信息弹窗

--- sync.sh
+++ sync.sh
@@ -3,6 +3,13 @@
 # File Name: sync.sh
 # Created Time: 2020年11月01日 星期日 00时38分28秒
 #########################################################################
-date=`date`
+print_date(){
+	date +'%Y/%m/%d/%R'
+}
+#备份音乐
 rclone copy /home/rewrite/Music/CloudMusic gd:/Music -v >> /tmp/log/sync.log 2>&1
-echo $date >> /tmp/log/sync.log
+#备份视频壁纸
+rclone copy /home/rewrite/Videos/live_wallpaper gd:/live_wallpaper -v >> /tmp/log/sync.log 2>&1
+
+echo `print_date`  备份完成
+zenity --info --text "sync定时任务完成"
