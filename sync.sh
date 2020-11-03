f9151f7a9c4d888698378624ffc734248af2767a
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-11-03 22:35:34 +0800

    --

--- sync.sh
+++ sync.sh
@@ -7,9 +7,9 @@ print_date(){
 	date +'%Y/%m/%d/%R'
 }
 #备份音乐
-rclone copy /home/rewrite/Music/CloudMusic gd:/Music -v >> /tmp/log/sync.log 2>&1
+rclone copy /home/rewrite/Music/CloudMusic gd:/Music -Pv --cache-chunk-size 128M > /tmp/log/sync.log 2>&1
 #备份视频壁纸
-rclone copy /home/rewrite/Videos/live_wallpaper gd:/live_wallpaper -v >> /tmp/log/sync.log 2>&1
+rclone copy /home/rewrite/Videos/live_wallpaper gd:/live_wallpaper -Pv --cache-chunk-size 128M > /tmp/log/sync.log 2>&1
 
 echo `print_date`  备份完成
 zenity --info --text "sync定时任务完成"
