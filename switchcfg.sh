6bf6fb6f38af4cb898c7db5671f3bf91358750c4
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-11-04 23:54:23 +0800

    crontab定时任务增加 - 21点切换clash配置

--- switchcfg.sh
+++ switchcfg.sh
@@ -5,6 +5,12 @@ cfg_dir=~/.config/clash
 #原配置文件路径
 file_cfg_dir=~/.config/clash/config.yaml
 #######################################################################
+print_date(){
+	date +'%Y/%m/%d/%R'
+}
+source /etc/profile
+export DISPLAY=:0.0
+#######################################################################
 ~/prog/subconverter/subconverter -g
 echo "配置下载完成"
 if [ -e ${new_file_cfg_dir} ];then
@@ -17,13 +23,19 @@ if [ -e ${new_file_cfg_dir} ];then
 		echo "准备启动clash"
 		~/scripts/clash.sh
 		echo "clash配置切换完成!"
+		echo "clash配置切换脚本已执行 -- 成功 -- "`print_date` > /tmp/log/scfg.log
+		zenity --info --width=200 --height=150 --text "clash配置切换定时任务已执行--成功" --timeout=5
 	else
 		echo "clash正在运行"
 		echo "准备重启clash"
 		pkill clash
 		~/scripts/clash.sh
 		echo "clash配置切换完成!"
+		echo "clash配置切换脚本已执行 -- 成功 -- "`print_date` > /tmp/log/scfg.log
+		zenity --info --width=200 --height=150 --text "clash配置切换定时任务已执行--成功" --timeout=5
 	fi	
 else
 	echo "配置文件不存在,退出脚本"
+	echo "clash配置切换脚本已执行 -- 失败 --- "`print_date` > /tmp/log/scfg.log
+	zenity --info --width=200 --height=150 --text "clash配置切换定时任务已执行--失败" --timeout=5
 fi
