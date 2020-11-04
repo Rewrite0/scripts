1a11782a460bad5e60a023b2215abf353c19aad6
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-11-04 23:47:55 +0800

    修复sync脚本弹窗失败的问题

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@
 # Created Time: 2020年11月01日 星期日 00时38分28秒
 #########################################################################
 source /etc/profile
+export DISPLAY=:0.0
 print_date(){
 	date +'%Y/%m/%d/%R'
 }
@@ -12,5 +13,5 @@ rclone copy /home/rewrite/Music/CloudMusic gd:/Music -Pv --cache-chunk-size 128M
 #备份视频壁纸
 rclone copy /home/rewrite/Videos/live_wallpaper gd:/live_wallpaper -Pv --cache-chunk-size 128M > /tmp/log/sync.log 2>&1
 
-echo `print_date`  备份完成
-zenity --info --text "sync定时任务完成"
+echo `print_date`  备份完成 >> /tmp/log/sync.log
+zenity --info --width=200 --height=150 --text "sync定时任务已执行" --timeout=10
