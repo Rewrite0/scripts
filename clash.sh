8b30fc4c793f0b0d8fe91d8517d808afef7ddc2c
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-01-28 22:37:09 +0800

    添加clash安装脚本

--- clash.sh
+++ clash.sh
@@ -4,6 +4,7 @@
 # Created Time: 2020年07月14日 星期二 18时04分23秒
 #########################################################################
 a=$1
+b=`find /bin /usr/bin /usr/local/bin | grep clash`
 start(){
 	nohup clash > /tmp/log/clash.log 2>&1 &
 	sleep 2
@@ -11,7 +12,15 @@ start(){
 }
 
 if [ -z $a ];then
-	start
+	if [ -z $b ];then
+		echo "clash未安装，准备下载安装。"
+		bash ./clash_install.sh
+		echo "安装完成，启动"
+		mkdir /tmp/log
+		start
+	else
+		start
+	fi
 elif [ $a == restart ];then
 	killall clash
 	start
