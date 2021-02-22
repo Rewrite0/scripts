e6ed65aeeb78b148ed52be7bfab70e9620629ce4
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-02-22 19:03:12 +0800

    --

--- mount-disk.sh
+++ mount-disk.sh
@@ -7,16 +7,16 @@ if [ $mode == m ];then
 	echo "查看磁盘信息"
 	sudo fdisk -l | grep /dev
 	read -p "输入磁盘位置(/dev后的位置):" disk
-	read -p "输入挂载位置(1/2/3):" dir
-	sudo mount -o uid=$user,gid=$user /dev/$disk /home/$user/disks/$dir
-	df -h | grep /disk
+	read -p "输入挂载位置(1/2):" dir
+	sudo mount -o uid=$user,gid=$user /dev/$disk /mnt/$dir
+	df -h | grep /mnt
 elif [ $mode == u ];then
-	if [ "$(df -h | grep /disk)" != "" ];then
+	if [ "$(df -h | grep /mnt)" != "" ];then
 		echo "查看挂载信息"
-		df -h | grep /disk
-		read -p "输入需卸载磁盘的挂载位置(1/2/3):" dir
-		sudo umount /home/$user/disks/$dir
-		df -h | grep /disk
+		df -h | grep /mnt
+		read -p "输入需卸载磁盘的挂载位置(1/2):" dir
+		sudo umount /mnt/$dir
+		df -h | grep /mnt
 	else
 		echo "无可卸载u盘,退出..."
 	fi
