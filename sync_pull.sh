6312d2f3345c810e135e2c6ed289b2f4d49b632f
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-03-08 20:41:48 +0800

    --

--- sync_pull.sh
+++ sync_pull.sh
@@ -3,9 +3,8 @@
 # File Name: pull_gd.sh
 # Created Time: 2021年03月02日 星期二 16时47分20秒
 #########################################################################
-rm /tmp/log/pull.log
 #pull音乐
-rclone copy mega:/Music /home/rewrite/Music -Pv --cache-chunk-size 128M >> /tmp/log/pull.log 2>&1
+rclone copy mega:/Music /home/rewrite/Music -Pv --cache-chunk-size 128M > /tmp/log/pull.log 2>&1
 #pull视频壁纸
 rclone copy mega:/live_wallpaper /home/rewrite/Videos/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/pull.log 2>&1
 
--- sync_push.sh
+++ sync_push.sh
@@ -3,14 +3,13 @@
 # File Name: push_gd.sh
 # Created Time: 2021年03月02日 星期二 16时46分20秒
 #########################################################################
-rm /tmp/log/push.log
 #push音乐
-rclone copy /home/rewrite/Music mega:/Music -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
-rclone copy /home/rewrite/Music od:/Music -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
-rclone copy /home/rewrite/Music od5t:/Music -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
+rclone copy /home/rewrite/Music mega:/Music -Pv --cache-chunk-size 128M > /tmp/log/push.log 2>&1
+rclone copy /home/rewrite/Music od:/Backups/Music -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
+rclone copy /home/rewrite/Music e5:/Backups/Music -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
 #push视频壁纸
 rclone copy /home/rewrite/Videos/live_wallpaper mega:/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
-rclone copy /home/rewrite/Videos/live_wallpaper od:/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
-rclone copy /home/rewrite/Videos/live_wallpaper od5t:/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
+rclone copy /home/rewrite/Videos/live_wallpaper od:/Backups/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
+rclone copy /home/rewrite/Videos/live_wallpaper e5:/Backups/live_wallpaper -Pv --cache-chunk-size 128M >> /tmp/log/push.log 2>&1
 
 echo `date` push完成 >> /tmp/log/push.log
